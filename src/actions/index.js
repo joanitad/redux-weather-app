c72ca3c8ab4d8dc98145f1bdb697b6674e419582
Joanita <jdsouza@insidesales.com>
2016-07-27 14:30:13 -0600

    Redux weather app using Sparklines and GoogleMaps

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_KEY = 'f5102bb71b7b9d2ebb0bf82b8a236d5b';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
